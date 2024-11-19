95e7a92147fb8768553f160664735a4984335e6c
Peter Andrén <piddah80@gmail.com>
2024-11-19 13:59:36 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine(GymMemberShip(34, false));
+
+
+
+int GymMemberShip(int age, bool premium)
+{
+    int membershipCost = 0;
+    if (premium) membershipCost = 300;
+
+    if (age <= 19 || age >= 64)
+    {
+        membershipCost += 2200;
+    }
+    else
+    {
+        membershipCost += 3100;
+    }
+    return membershipCost;
+}
